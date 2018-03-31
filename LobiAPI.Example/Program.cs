f57586bd2900d6700b7c3a180db0a076e44d6829
NewDelion <el.emena.crea@gmail.com>
2018-03-31 22:25:17 +0900

    ログインのサンプルプロジェクトを追加

---
+++ LobiAPI.Example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LobiAPI.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = new BasicAPI();
+            Login(api);
+        }
+
+        static void Login(BasicAPI api)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Mail: ");
+                string mail = Console.ReadLine();
+                Console.Write("Password: ");
+                string password = Console.ReadLine();
+
+                if (api.Login(mail, password))
+                    break;
+                Console.WriteLine("ログイン失敗....");
+                Console.ReadKey();
+            }
+            Console.WriteLine("ログインしました。");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
